e6f5f8e415680caf7fd99ef709bb9ed733dbfd88
Travis Vadnais <travisvadnais@gmail.com>
2018-07-07 22:16:20 -0400

    update folder structure

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const router = require('express').Router();
+
+router.get("/scrape", function(req, res) {
+
+    const url = "https://www.vivino.com/explore?e=eJzLLbI11jNVy83MszU0UMtNrLA1MVBLrrQtLVZLtg0NdlErsDVUS0-zLUssykwtScxRy022VctPAmLblNTiZLXykuhYoIpi27QcAHJ8F-Q=";
+    request(url, function(error, res, body) {
+        //Shoot the page into Cheerio
+        if (error) return console.log(error);
+        const $ = cheerio.load(body);
+
+        $(".explorerCard__rightColumn--1ZpZ7").each(function(i, element) {
+            let title = $(".explorerCard__rightColumn--1ZpZ7").children().first().html();
+            console.log(title);
+        })
+    })
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,35 @@
+//Set up the required packages
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3010;
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var allRoutes = require("./routes/routes.js");
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news_scraper";
+
+
+
+//========= Configure all the Express stuff =======================//
+
+//Morgan will log requests
+app.use(logger("dev"));
+//BodyParser will handle form submissions
+app.use(bodyParser.urlencoded({extended: true}));
+//Serve public folder as static directory
+app.use(express.static("public"));
+app.use('/', allRoutes);
+
+//====================== End Express Stuff ========================//
+
+//mongoose.Promise = Promise;
+//mongoose.connect(MONGODB_URI).catch(err => console.log(err));
+
+//Link up w/ MongoDB
+//mongoose.connect("mongodb://localhost/news_scraper");
+
+//Start Server
+app.listen(PORT, function() {
+    console.log(`You launched port: ${PORT}!`)
+})
