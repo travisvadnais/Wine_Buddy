1be68a6e2832fdbf62bce79ea5068912a8f6bc62
Travis Vadnais <travisvadnais@gmail.com>
2018-07-15 18:05:48 -0400

    update props on dropdown

--- wine_buddy/src/components/Dropdown/Dropdown.js
+++ wine_buddy/src/components/Dropdown/Dropdown.js
@@ -6,7 +6,7 @@ import foods from '../../pages/Wine/foods.json'
 const Dropdown = (props) => (
   <select name="food">
     {/* Default Option */}
-    <option value="" disabled selected>Select the Star of Your Dish!</option>
+    <option value="" disabled selected>{props.placeholder}</option>
     {/* We're going to loop through each item in the foods API and set up an Option for each one */}
     {foods.map(e =>{
       return <option key={e.food} value={e.food}>{e.food}</option>;
