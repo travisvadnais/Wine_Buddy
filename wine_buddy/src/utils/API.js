dab9c764f2a7838a3addfdc69d40ec316d7d48c2
ksouders92 <ksouders@knights.ucf.edu>
2018-07-19 16:07:07 -0400

    handleFormSubmit

--- wine_buddy/src/utils/API.js
+++ wine_buddy/src/utils/API.js
@@ -4,7 +4,7 @@ export default {
 // get wine to return wines that fit the foodselection, wineselection, and price range upon handleformsubmit
 // named getWines?
   getWines: function(food) {
-    return axios.get(`/api/wine/${food}`);
+    return axios.get(`/api/wine/:${food}`);
   },
 
 
