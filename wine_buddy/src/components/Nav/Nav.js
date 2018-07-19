f09fd589084b2a3ee81fe8ee7d343fdc129344f1
ksouders92 <ksouders@knights.ucf.edu>
2018-07-19 05:17:14 -0400

    format list

--- wine_buddy/src/components/Nav/Nav.js
+++ wine_buddy/src/components/Nav/Nav.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Nav = props => (
-    <nav className="navbar navbar-expand-lg navbar-brand navbar-light bg-light justify-content-between">WineBuddy
+    <nav className="navbar navbar-expand-lg navbar-brand navbar-light bg-light justify-content-between">
+    <h1 className="brand">Wine Buddy</h1>
+    <br/>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-                <li className="nav-item active">
+                <li className="nav-item">
                     {/* The 'Link to' will render the linked page */}
                     <Link to="../home" className="nav-link" id="go_home" href="">Home <span className="sr-only">(current)</span></Link>
                 </li>
                 <li className="nav-item">
                     <Link to="../profile" id="saved_articles" className="nav-link" href="">My Wines</Link>
                 </li>
-                <Link to="../wine_pair"><button type="button" className="btn btn-success" id="scraper">Find Your Wine!</button></Link>
             </ul>
+                <Link to="../wine_pair"><button type="button" className="btn btn-danger" id="scraper">Find Your Wine!</button></Link>
         </div>
        
     </nav>
