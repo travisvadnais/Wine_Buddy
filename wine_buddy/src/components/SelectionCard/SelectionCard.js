987a9a09c7793086bf38fe6b96e5c335f63ed0f1
Travis Vadnais <travisvadnais@gmail.com>
2018-07-20 22:30:22 -0400

    update selection cards w/ state and unique images.  update styling on the cards

--- wine_buddy/src/components/SelectionCard/SelectionCard.js
+++ wine_buddy/src/components/SelectionCard/SelectionCard.js
@@ -1,13 +1,95 @@
-import React from 'react';
-import './SelectionCard.css';
 
-const SelectionCard = props => (
-  <div className="card user_card">
-    <img className="card-img-top user_selection" src={props.image} alt={props.alt} />
+import React, { Component } from "react";
+import './SelectionCard.css';
+import pricePics from './priceImages';
+import winePics from './wineImages';
+import foodPics from './foodImages';
+
+
+class SelectionCard extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        src: ""
+      }
+      this.findImage = this.findImage.bind(this);
+    }
+  
+  //Find the image as soon as the component is mounted
+  componentDidMount() {this.findImage()}
+
+  //Also find the image when the component gets updated w/ new props (I.E. user changes the dropdown)
+  componentDidUpdate(prevProps){
+    if (this.props.selection !== prevProps.selection) { this.findImage()}
+  };
+  
+  //I just put all the different wines; prices; and foods in a switch
+  findImage() {
+    switch (this.props.selection) {
+      case "under $20": 
+        this.setState({src: pricePics[0]})
+        break;
+      case "$20-$40":
+        this.setState({src: pricePics[1]})
+        break;
+      case "$41-$80":
+        this.setState({src: pricePics[2]})
+        break;
+      case "over $80":
+        this.setState({src: pricePics[3]})
+        break;
+      case "Dry Whites":
+      case "Sparkling Whites":
+      case "Sweet Whites":
+      case "Rich Whites":
+        this.setState({src: winePics[0]})
+        break;
+      case "Dessert":
+        this.setState({src: winePics[1]})
+        break;
+      case "Medium Body Reds":
+      case "Full Body Reds":
+      case "Light Body Reds":
+        this.setState({src: winePics[2]})
+        break;
+      case "Vegetables":
+        this.setState({src: foodPics[0]})
+        break;
+      case "Soft Cheese":
+      case "Hard Cheese":
+        this.setState({src: foodPics[1]})
+        break;
+      case "Starches (Grains & Pastas)":
+        this.setState({src: foodPics[2]})
+        break;
+      case "Light Seafood":
+      case "Rich Seafood":
+        this.setState({src: foodPics[3]})
+        break;
+      case "White Meat":
+      case "Red Meat":
+      case "Cured Meat":
+        this.setState({src: foodPics[4]})
+        break;
+      case "Sweets":
+        this.setState({src: foodPics[5]})
+        break;
+      default:
+        this.setState({src: "No Picture"})
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div className="card user_card">
+        <img className="card-img-top user_selection" src={this.state.src} alt={this.props.alt} />
         <div className="card-body">
-            <p className="card-text">{props.selection}</p>
+          <p className="card-text">{this.props.selection}</p>
         </div>
-  </div>
-)
+      </div>
+    )
+  }
+}
 
 export default SelectionCard
---
+++ wine_buddy/src/components/SelectionCard/foodImages.js
@@ -0,0 +1,10 @@
+const foodImages = [
+  "https://www.organicfacts.net/wp-content/uploads/vegetarianfood.jpg", //veggies[0]
+  "https://www.cheesehouse.com/wp-content/uploads/2017/05/hero-cheese.png", //cheese[1]
+  "https://images.wisegeek.com/grains.jpg", //starches[2]
+  "https://img.taste.com.au/JNYsCm2d/w720-h480-cfill-q80/taste/2016/11/grilled-seafood-platter-81225-1.jpeg",
+  "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/06/30/10/istock-483027918.jpg?w968h681",
+  "https://selectsandwich.com/wp-content/uploads/2014/12/dessert_platter1.jpg" //desserts[5]
+]
+
+export default foodImages;
