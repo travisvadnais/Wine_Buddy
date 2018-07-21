6192b44dce179f449bcdf3b8cbcbc5e1cf557762
Travis Vadnais <travisvadnais@gmail.com>
2018-07-20 22:39:21 -0400

    uncomment code in API.js to avoid conflicts

--- wine_buddy/src/utils/API.js
+++ wine_buddy/src/utils/API.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 export default {
 // get wine to return wines that fit the foodselection, wineselection, and price range upon handleformsubmit
 // named getWines?
-  // getWines: function(food) {
-  //   return axios.get(`/api/wine/:${food}`);
-  // },
+  getWines: function(food) {
+    return axios.get(`/api/wine/:${food}`);
+  },
 
 
   //Travis fx
