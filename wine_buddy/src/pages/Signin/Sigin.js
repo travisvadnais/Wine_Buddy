43c363c0912b1f699489fa080dabfdf473561104
ksouders92 <ksouders@knights.ucf.edu>
2018-07-21 01:55:33 -0400

    signout button

--- wine_buddy/src/pages/Signin/Sigin.js
+++ wine_buddy/src/pages/Signin/Sigin.js
@@ -1,22 +1,98 @@
 import React, { Component } from "react";
 import Nav from "../../components/Nav";
-import SigninPage from "../../components/SigninPage";
+import Signin from "../../components/Signin";
 import Footer from '../../components/Footer';
 import './Signin.css';
 // import API from "../../utils/API";
 import { Link } from "react-router-dom";
 
-class Signin extends Component {
-    render() {
-        return (
-            <div>
-                <Nav />
-                {/* I built this class stack in another HW and it works for this type of thing */}
-                <h3>This is where the Sign in form will go</h3>
-                <Footer />
-            </div>
-        );
-    }
+import { SignUpLink } from './SignUp';
+import { auth } from '../firebase';
+import * as routes from '../constants/routes';
+
+const SignInPage = ({ history }) =>
+  <div>
+    <h1>SignIn</h1>
+    <SignIn history={history} />
+    <SignUpLink />
+  </div>
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null,
+};
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const {
+      email,
+      password,
+    } = this.state;
+
+    const {
+      history,
+    } = this.props;
+
+    auth.doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState(() => ({ ...INITIAL_STATE }));
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error));
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    const {
+      email,
+      password,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      password === '' ||
+      email === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={password}
+          onChange={event => this.setState(byPropKey('password', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    );
+  }
 }
 
-export default Signin
+export default withRouter(SignIn);
+
+export {
+  SignIn,
+};
--- wine_buddy/src/pages/Signup/Signup.js
+++ wine_buddy/src/pages/Signup/Signup.js
@@ -5,18 +5,121 @@ import Footer from '../../components/Footer';
 import './Signup.css';
 // import API from "../../utils/API";
 import { Link } from "react-router-dom";
+import { auth } from '../firebase';
 
-class Signup extends Component {
+const SignUpPage = ({ history }) =>
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm history={history} />
+    </div>
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value,
+});
+
+class SignUpForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = (event) => {
+        const {
+            username,
+            email,
+            passwordOne,
+          } = this.state;
+
+          const {
+            history,
+          } = this.props;
+      
+          auth.doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+              this.setState(() => ({ ...INITIAL_STATE }));
+              history.push(routes.HOME);
+            })
+            .catch(error => {
+              this.setState(byPropKey('error', error));
+            });
+      
+          event.preventDefault();
+
+    }
+
     render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
+
+        const isInvalid =
+            passwordOne !== passwordTwo ||
+            passwordOne === '' ||
+            email === '' ||
+            username === '';
         return (
-            <div>
-                <Nav />
-                {/* I built this class stack in another HW and it works for this type of thing */}
-                <h3>This is where the Signup form will go</h3>
+
+            <div> <Nav />
+
+                <form onSubmit={this.onSubmit}>
+                    <input
+                        value={username}
+                        onChange={event => this.setState(byPropKey('username', event.target.value))}
+                        type="text"
+                        placeholder="Full Name"
+                    />
+                    <input
+                        value={email}
+                        onChange={event => this.setState(byPropKey('email', event.target.value))}
+                        type="text"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        value={passwordOne}
+                        onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        value={passwordTwo}
+                        onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
+                    <button disabled={isInvalid} type="submit">
+                        Sign Up
+        </button>
+
+                    {error && <p>{error.message}</p>}
+                </form>
                 <Footer />
             </div>
         );
     }
 }
 
-export default Signup
+const SignUpLink = () =>
+    <p>
+        Don't have an account?
+    {' '}
+        <Link to={routes.SIGN_UP}>Sign Up</Link>
+    </p>
+
+export default withRouter (SignUp);
+
+export {
+    SignUpForm,
+    SignUpLink,
+};
