c563c11e6d8c1b0a607158042c7c91487b5a9cf5
ksouders92 <ksouders@knights.ucf.edu>
2018-07-24 00:14:36 -0400

    change price images

--- wine_buddy/src/components/SelectionCard/priceImages.js
+++ wine_buddy/src/components/SelectionCard/priceImages.js
@@ -1,7 +1,7 @@
 const priceImages = [
-  "http://churchfurniturepartner.com/wp-content/uploads/2013/10/cheap-led-signs1.jpg",
+  "http://popcultureplaypen.files.wordpress.com/2011/12/i-love-cheap-wine.jpg",
   "https://www.sterlingwineonline.com/images/items/wine-fund-barrel.jpg",
-  "https://previews.123rf.com/images/loopall/loopall1002/loopall100200026/6420260-bag-with-packs-of-dollars-money-vector-illustration-isolated-on-white-background.jpg",
+  "http://www.housesellingspecialists.com/wp-content/uploads/2018/05/1.jpg",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFTILJAwS8vpfB0ELZto7HegUdKJK5pX8qhHRVZ2e-obD7tmMA"
 ]
 
